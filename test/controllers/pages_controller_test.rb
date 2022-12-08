8ced2d56111314532b70db566c7ba0b1360bb410
Gui Recordon <guilherme.recordon@gmail.com>
2022-12-07 23:02:00 -0300

    first commit

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get stats" do
+    get pages_stats_url
+    assert_response :success
+  end
+end
